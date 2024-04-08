80dca2d75d9090778869f4e6942ec75459a2d454
“Shannon <Shannon.dutton@shopify.com>
2024-04-08 17:22:46 +1000

    doco update

--- extensions/four-item-25off/src/run.rs
+++ extensions/four-item-25off/src/run.rs
@@ -1,7 +1,17 @@
 /*
-This is a Product Discount Function and check if the quantity of line item is 4 or not and applys a 25% discount, this applies across all line items where condition is met. 
-It also has an extra optional condition of checking it a customer tag is applied before adding the discount. This condition is not needed. 
-It will apply ALL discounts(Discount Strategy) that apply not just the FIRST one. 
+Main function to apply discounts.
+
+    This function applies a 25% discount to line items if the quantity is 4 only. 
+    It first checks if the customer has a VIP tag. If not, no discount is applied.
+    If the customer is a VIP, the function iterates over all line items and checks if the quantity of each item is 4 only. If the condition is met, it applies a 25% discount to the line item. 
+
+    The function follows a 'Discount Strategy' that applies all applicable discounts, not just the first one. This means if multiple conditions for discounts are met, all of them will be applied.
+    # Arguments 
+    * `input` - A ResponseData object containing the cart details.
+
+    # Returns
+    * `FunctionRunResult` - A result object containing the applied discounts and the discount application strategy.
+
 Discount Application Strategy Dev Docs(https://shopify.dev/docs/api/functions/reference/product-discounts/graphql/common-objects/discountapplicationstrategy?api%5Bversion%5D=2024-07)
 
 */
@@ -47,6 +57,19 @@ fn run(input: run::input::ResponseData) -> Result<run::output::FunctionRunResult
     })
 }
 
+/* 
+Helper function to calculate discounts.
+
+This function iterates over all line items and checks if the quantity of each item 
+is equal to 4. If the condition is met, it applies a 25% discount to the line item.
+
+Arguments:
+* `lines` - A vector of InputCartLines objects representing the line items in the cart.
+
+Returns:
+* `Vec<Discount>` - A vector of Discount objects representing the applied discounts.
+
+*/
 fn get_discounts(lines: &Vec<run::input::InputCartLines>) -> Vec<run::output::Discount> {
     let mut result: Vec<run::output::Discount> = Vec::new();
 
