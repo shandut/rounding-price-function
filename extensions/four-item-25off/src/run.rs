d9b3d48dfd080c7fa801ac705b63d886f18ee557
“Shannon <Shannon.dutton@shopify.com>
2024-04-08 17:04:14 +1000

    optimised if statments and clone id

--- extensions/four-item-25off/src/run.rs
+++ extensions/four-item-25off/src/run.rs
@@ -4,12 +4,9 @@ use serde::{Deserialize, Serialize};
 use rust_decimal::Decimal;
 use std::str::FromStr;
 
-
 #[derive(Serialize, Deserialize, Default, PartialEq)]
 #[serde(rename_all(deserialize = "camelCase"))]
 struct Configuration {}
-
-
 
 #[shopify_function_target(query_path = "src/run.graphql", schema_path = "schema.graphql")]
 fn run(input: run::input::ResponseData) -> Result<run::output::FunctionRunResult> {
@@ -18,25 +15,19 @@ fn run(input: run::input::ResponseData) -> Result<run::output::FunctionRunResult
         discount_application_strategy: run::output::DiscountApplicationStrategy::ALL,
     };
 
-    //No nessasary however this is an extra condition that a customer has a tag that must be true before the discount applies.
-    let vip = match input.cart.buyer_identity {
-        Some(identity) => match identity.customer {
-            Some(customer) => match customer.has_any_tag {
-                true => true,
-                false => false,
-            },
-            None => {
-                eprintln!("No tag for VIP");
-                return Ok(no_discount);
-            }
-        },
-        None => {
-            eprintln!("No cart buyer identity found");
+    let vip = if let Some(identity) = input.cart.buyer_identity {
+        if let Some(customer) = identity.customer {
+            customer.has_any_tag
+        } else {
+            eprintln!("No tag for VIP");
             return Ok(no_discount);
         }
+    } else {
+        eprintln!("No cart buyer identity found");
+        return Ok(no_discount);
     };
 
-    if vip == false {
+    if !vip {
         eprintln!("User is not VIP");
         return Ok(no_discount);
     }
@@ -53,37 +44,25 @@ fn get_discounts(lines: &Vec<run::input::InputCartLines>) -> Vec<run::output::Di
     let mut result: Vec<run::output::Discount> = Vec::new();
 
     for line in lines.iter() {
-        //Add apply logic here, in this case we are applying to all line items. But you can tailor this to specific product of collections depending on the conditions you want the discount code to work for.
         if line.quantity == 4 {
-            match &line.merchandise {
-                run::input::InputCartLinesMerchandise::ProductVariant(variant) => {
-                    let target = run::output::Target::ProductVariant(run::output::ProductVariantTarget {
-                        id: variant.id.clone(),
-                        quantity: None,
-                    });
+            if let run::input::InputCartLinesMerchandise::ProductVariant(variant) = &line.merchandise {
+                let target = run::output::Target::ProductVariant(run::output::ProductVariantTarget {
+                    id: variant.id.clone(),
+                    quantity: None,
+                });
 
-                    // Calculate the discount amount 
-                    //This example is discounting a fixed amount 15% 
-
-                   // let ppp = line.cost.amount_per_quantity.amount; // get current price
-                   // let ppp_f64 = ppp.as_f64(); // convert to floating point
-                   // let discount_amount = (ppp_f64 * 0.15).ceil(); //round up because you want the highest $ amount discount off.
-                   
-                    let discount = run::output::Discount {
-                        message: None,
-                        targets: vec![target],
-                        value: run::output::Value::Percentage(run::output::Percentage {
-                            value: Decimal::from_str("25.0").unwrap()
-                    
-                        }),
-                    };
-                  
-                    result.push(discount);
-                },
-                _ => {}
+                let discount = run::output::Discount {
+                    message: None,
+                    targets: vec![target],
+                    value: run::output::Value::Percentage(run::output::Percentage {
+                        value: Decimal::from_str("25.0").unwrap()
+                    }),
+                };
+              
+                result.push(discount);
             }
         }
     }
 
-    return result;
+    result  //returns result
 }
